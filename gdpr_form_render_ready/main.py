31741571728aba18db26b5655f2351da8f73d58d
christergrundstrom-cpu <christer.grundstrom@gmail.com>
2025-08-04 17:55:00 +0200

    Add files via upload

--- gdpr_form_render_ready/main.py
+++ gdpr_form_render_ready/main.py
@@ -1,1 +1,10 @@
-print('GDPR Form Server Startad')
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "GDPR-formulär är online!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
